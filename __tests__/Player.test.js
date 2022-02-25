bcb53834b9d2d2ddad7efffe6445d090aca0bca2
Sara Gochez <saragochez13@gmail.com>
2022-02-25 00:45:00 -0800

    modified Player function

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,7 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
+//console.log(new Potion());
 
 const Player = require('../lib/Player.js');
 
@@ -15,4 +15,24 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)])
     );
+})
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+// this simulates and empty array
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 })
